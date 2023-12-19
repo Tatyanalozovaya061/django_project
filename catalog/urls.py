bfad9c07816aa31b52c1d82eea08de4db794c909
tatyana lozovaya <tatka8632@gmail.com>
2023-12-19 21:51:31 +0300

    initial

---
+++ catalog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from catalog.views import index, contact, home
+
+urlpatterns = [
+    # path('', index),
+    # path('', contact),
+    path('', home),
+]
---
+++ catalog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        if name is not None and email is not None:
+            return render(request, 'catalog/contacts.html', context={'name': name, 'email': email})
+    return render(request, 'catalog/contacts.html')
+
+def home(request):
+    return render(request, 'catalog/home.html')
