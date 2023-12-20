9e1cfd683b58e3e644ff3b9d1d9a26b77f951517
tatyana lozovaya <tatka8632@gmail.com>
2023-12-20 15:10:38 +0300

    добавление путей

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from catalog.views import index, contact, home
 
 urlpatterns = [
-    # path('', index),
-    # path('', contact),
+    path('', index),
+    path('', contact),
     path('', home),
 ]
